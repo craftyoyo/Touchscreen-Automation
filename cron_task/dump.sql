87756a0b4f5e9607f219d463303ceeb4304119ec
AiGreek <DrWhat@iMac-de-DrWhat.local>
2016-01-16 17:20:09 +0100

    First Commit
    First Commit

--- cron_task/dump.sql
+++ cron_task/dump.sql
@@ -1,28 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4499
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Hôte: 192.168.0.2 (MySQL 5.5.44-0+deb8u1)
-# Base de données: Raspberry
-# Temps de génération: 2016-01-16 13:54:17 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Affichage de la table cpu
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `cpu`;
 
 CREATE TABLE `cpu` (
@@ -31,11 +6,6 @@ CREATE TABLE `cpu` (
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-# Affichage de la table ping
-# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `ping`;
 
@@ -49,11 +19,6 @@ CREATE TABLE `ping` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Affichage de la table sensors
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `sensors`;
 
 CREATE TABLE `sensors` (
@@ -63,13 +28,3 @@ CREATE TABLE `sensors` (
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
